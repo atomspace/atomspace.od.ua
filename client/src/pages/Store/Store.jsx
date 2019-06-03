20637d160de251fce856790037d358e27ebc2908
vladimir.s <vladimi.s@mserverone.com>
2019-06-03 10:40:47 +0300

    If no one merch

--- client/src/pages/Store/Store.jsx
+++ client/src/pages/Store/Store.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Slider from "react-slick";
 
 import Arrow from "../../components/Arrow";
 import MerchSize from "../../pages/Store/MerchSize";
 import MerchBuy from "../../pages/Store/MerchBuy";
-import { getAllMerches } from "../../api/merch";
-import { MEDIA_URL } from "../../utils/config";
+import {getAllMerches} from "../../api/merch";
+import {MEDIA_URL} from "../../utils/config";
 
 class Store extends Component {
   state = {
@@ -27,9 +27,11 @@ class Store extends Component {
 
   async componentDidMount() {
     let merches = await getAllMerches();
-    merches = this.increaseCountOfMerch(merches);
-    this.props.changeMerchAttr(this.props.getCachedMerch() || merches[0].fields);
-    this.setState({ merches: merches.map(merch => merch.fields) });
+    if (merches.length) {
+      merches = this.increaseCountOfMerch(merches);
+      this.props.changeMerchAttr(this.props.getCachedMerch() || merches[0].fields);
+      this.setState({merches: merches.map(merch => merch.fields)});
+    }
   }
 
   render() {
@@ -43,15 +45,15 @@ class Store extends Component {
         slidesToShow: 3,
         slidesToScroll: 1,
         arrows: true,
-        prevArrow: <Arrow rotate />,
-        nextArrow: <Arrow />,
+        prevArrow: <Arrow rotate/>,
+        nextArrow: <Arrow/>,
         focusOnSelect: true,
         swipe: true,
         adaptiveHeight: true,
         swipeToSlide: true,
         centerMode: true,
         afterChange: index => {
-          this.setState({ index });
+          this.setState({index});
           const merch = this.state.merches[index] || this.state.merches[0];
           this.props.changeMerchAttr(merch);
         }
@@ -73,7 +75,7 @@ class Store extends Component {
                 <div key={index} className="carousel-block">
                   <div className="carousel-info__merch">
                     <div className="wrapper">
-                      <div className="ellipse" />
+                      <div className="ellipse"/>
                       <img
                         className="merch-logo"
                         src={`${MEDIA_URL}/${merch.avatar_url}`}
@@ -86,7 +88,7 @@ class Store extends Component {
             </Slider>
           </div>
           <div className="store-buttons-mobile">
-            <MerchSize changeMerchAttr={this.props.changeMerchAttr} />
+            <MerchSize changeMerchAttr={this.props.changeMerchAttr}/>
             <MerchBuy
               order={this.props.order}
               handleDialog={this.props.handleDialog}
