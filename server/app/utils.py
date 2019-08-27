12d9d828dc1cc5044c4e5f53281c1b6c8960fa09
Volodymyr Saakian <vsaakian@lohika.com>
2019-08-27 12:59:42 +0300

    Resolve conflicgts

--- server/app/utils.py
+++ server/app/utils.py
@@ -13,8 +13,4 @@ class EmailThread(threading.Thread):
     def run(self):
         msg = EmailMessage(self.subject, self.html_content,
                            self.from_email, self.recipient_list)
-<<<<<<< HEAD
-=======
-        # msg.content_subtype = "html"
->>>>>>> 5a91d1ba8d356c84c843bddf110a6ff821086598
         msg.send()
