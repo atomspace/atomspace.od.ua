0cc7ce25f8b0575ed3ada3e1e2352da048a8246c
Mishawaka <you@example.com>
2019-11-24 22:53:34 +0200

    Changed way of exporting data

--- server/app/admin.py
+++ server/app/admin.py
@@ -1,27 +1,24 @@
 from django.contrib import admin
 from .models import Merch, News, Mentor, Resident, Order
-
+from import_export.admin import ExportActionMixin
 
 @admin.register(Merch)
 class MerchAdmin(admin.ModelAdmin):
     pass
 
-
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
     pass
 
-
 @admin.register(Mentor)
-class MentorAdmin(admin.ModelAdmin):
-    pass
-
+class MentorAdmin(ExportActionMixin, admin.ModelAdmin):
+    class Meta:
+        exclude = ('id', 'created_time', 'updated_time')
 
 @admin.register(Resident)
-class ResidentAdmin(admin.ModelAdmin):
+class ResidentAdmin(ExportActionMixin, admin.ModelAdmin):
     pass
 
-
 @admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
+class OrderAdmin(ExportActionMixin, admin.ModelAdmin):
     pass
