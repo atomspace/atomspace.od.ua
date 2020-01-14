bab544fc0049105f0e9c021a8e6bda705c276eab
Volodymyr Saakian <vsaakian@lohika.com>
2020-01-14 16:17:59 +0200

    Delete admin-client from front

--- admin-client/src/routes/LoginPage/LoginPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import LoginForm from '../../components/LoginForm/LoginForm';
-import './LoginPage.scss';
-
-export const LoginPage = () => (
-    <div className='login-page'>
-        <LoginForm/>
-    </div>
-);
